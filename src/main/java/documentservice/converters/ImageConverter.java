eff2afeb2de5b1a99c12b028a5acb8178d6563d9
Alexander Finn <finnam@gmail.com>
2015-07-31 19:16:03 +0300

    ImageConverter introduced

---
+++ src/main/java/documentservice/converters/ImageConverter.java
@@ -0,0 +1,20 @@
+package documentservice.converters;
+
+import documentservice.metadata.DocumentMetadata;
+
+import java.util.Properties;
+
+/**
+ * @author Alexander Finn
+ */
+public class ImageConverter implements Converter {
+  @Override
+  public void convert(DocumentMetadata metadata) {
+
+  }
+
+  @Override
+  public void configure(Properties settings) {
+
+  }
+}
